83d4e6d039ee1e4a3227b5664ba556dd0092eca1
Ashita Goyal <goyalashita99@gmail.com>
2022-08-22 20:45:24 +0530

    basic routing added using RouterModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'shopping-recipe-demo';
 
-  showPage = "recipe";
-
   constructor() {}
-
-  onSelectedPage(event) {
-    this.showPage = event;
-  }
 }
